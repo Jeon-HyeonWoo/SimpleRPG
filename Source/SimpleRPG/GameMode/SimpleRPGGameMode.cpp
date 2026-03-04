835ab847d342b50d172bcfec0357f6b8a4a51095
현우 전 <jhw096@gmail.com>
2026-03-05 05:06:10 +0900

    feat : Implement PawnData flow in GameMode

--- Source/SimpleRPG/GameMode/SimpleRPGGameMode.cpp
+++ Source/SimpleRPG/GameMode/SimpleRPGGameMode.cpp
@@ -2,10 +2,60 @@
 
 
 #include "SimpleRPGGameMode.h"
+#include "../Character/PawnData.h"
 #include "../Player/SimpleRPGPlayerState.h"
 
 ASimpleRPGGameMode::ASimpleRPGGameMode(const FObjectInitializer& ObjectInitializer) 
 	: Super(ObjectInitializer)
 {
 	PlayerStateClass = ASimpleRPGPlayerState::StaticClass();
+}
+
+void ASimpleRPGGameMode::HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer)
+{
+	ASimpleRPGPlayerState* PS = NewPlayer->GetPlayerState<ASimpleRPGPlayerState>();
+
+	if (IsValid(PS))
+	{
+		if (IsValid(DefaultPawnData))
+		{
+			PS->PawnData = DefaultPawnData;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("%d, %hs, DefaultPawnClass is inValid"), __LINE__, __FUNCTION__);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%d, %hs, ASimpleRPGPlayerState is inValid"), __LINE__, __FUNCTION__);
+	}
+
+
+	Super::HandleStartingNewPlayer_Implementation(NewPlayer);
+}
+
+UClass* ASimpleRPGGameMode::GetDefaultPawnClassForController_Implementation(AController* InController)
+{
+	ASimpleRPGPlayerState* PS = InController->GetPlayerState<ASimpleRPGPlayerState>();
+	
+	if (IsValid(PS))
+	{
+		UPawnData* PawnData = PS->PawnData;
+		if (IsValid(PawnData))
+		{
+			return PawnData->PawnClass;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("%d, %hs, UPawnData Is invalid"), __LINE__, __FUNCTION__);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%d, %hs, ASimpleRPGPlayerState is invalid"), __LINE__, __FUNCTION__);
+	}
+
+	return Super::GetDefaultPawnClassForController_Implementation(InController);
+
 }
--- Source/SimpleRPG/GameMode/SimpleRPGGameMode.h
+++ Source/SimpleRPG/GameMode/SimpleRPGGameMode.h
@@ -11,7 +11,11 @@
  */
 
 class ASimpleRPGPlayerState;
-
+class UPawnData;
+/*
+* 게임 규칙을 결정하는 GameMode
+* 서버 권위적으로 PawnData를 PlayerState에 전달
+*/
 UCLASS()
 class SIMPLERPG_API ASimpleRPGGameMode : public AGameModeBase
 {
@@ -21,4 +25,15 @@ public:
 
 	ASimpleRPGGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
+public:
+
+	/* 새 플레이어 참가 시 PawnData를 PlayerState에 전달 */
+	virtual void HandleStartingNewPlayer_Implementation(APlayerController* NewPlayer) override;
+	virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+public:
+
+	/* 이 게임모드에서 사용할 기본 PawnData */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "SimpleRPG|Pawn")
+	TObjectPtr<UPawnData> DefaultPawnData;
 };
