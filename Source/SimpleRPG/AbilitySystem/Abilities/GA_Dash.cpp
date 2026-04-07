08533c4a6cd6b58f19a7a5a58dc6ebfd3dcf3270
현우 전 <jhw096@gmail.com>
2026-04-07 22:23:03 +0900

    feat : GA_Dash 기본 구현(이동방향, LaunchCharacter, MontageChain)

---
+++ Source/SimpleRPG/AbilitySystem/Abilities/GA_Dash.cpp
@@ -0,0 +1,133 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA_Dash.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UGA_Dash::UGA_Dash()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Dashing")));
+
+	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Dashing")));
+}
+
+void UGA_Dash::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* Actorinfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!CommitAbility(Handle, Actorinfo, ActivationInfo))
+	{
+		EndAbility(Handle, Actorinfo, ActivationInfo, true, true);
+		return;
+	}
+
+	ACharacter* Character = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+
+	if (!Character)
+	{
+		EndAbility(Handle, Actorinfo, ActivationInfo, true, true);
+		return;
+	}
+
+	FVector DashDirection = GetDashDirection();
+
+	if (!DashDirection.IsNearlyZero())
+	{
+		FRotator DashRotation = DashDirection.Rotation();
+		Character->SetActorRotation(FRotator(0.0f, DashRotation.Yaw, 0.0f));
+	}
+
+	if (DashMontage)
+	{
+		UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+			this,
+			NAME_None,
+			DashMontage
+		);
+
+		MontageTask->OnCompleted.AddDynamic(this, &UGA_Dash::OnMontageCompleted);
+		MontageTask->OnBlendOut.AddDynamic(this, &UGA_Dash::OnMontageCompleted);
+		MontageTask->OnInterrupted.AddDynamic(this, &UGA_Dash::OnMontageCancelled);
+		MontageTask->OnCancelled.AddDynamic(this, &UGA_Dash::OnMontageCancelled);
+
+		MontageTask->ReadyForActivation();
+	}
+
+	float DashSpeed = DashDistance / DashDuration;
+	Character->LaunchCharacter(DashDirection * DashSpeed, true, true);
+
+}
+
+void UGA_Dash::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* Actorinfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	if (BlockMovementEffectHandle.IsValid())
+	{
+		GetAbilitySystemComponentFromActorInfo()->RemoveActiveGameplayEffect(BlockMovementEffectHandle);
+	}
+
+	Super::EndAbility(Handle, Actorinfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UGA_Dash::OnMontageCompleted()
+{
+	EndAbility(
+		CurrentSpecHandle,
+		CurrentActorInfo,
+		CurrentActivationInfo,
+		true,
+		false
+	);
+}
+
+void UGA_Dash::OnMontageCancelled()
+{
+	EndAbility(
+		CurrentSpecHandle,
+		CurrentActorInfo,
+		CurrentActivationInfo,
+		true,
+		true
+	);
+}
+
+FVector UGA_Dash::GetDashDirection() const
+{
+	ACharacter* Character = Cast<ACharacter>(GetAvatarActorFromActorInfo());
+
+	if (!Character)
+	{
+		return FVector::ZeroVector;
+	}
+
+	AController* Controller = Character->GetController();
+
+	if (!Controller)
+	{
+		return Character->GetActorForwardVector();
+	}
+
+	FRotator ControlRotation = Controller->GetControlRotation();
+	FRotator YawRotation(0.0f, ControlRotation.Yaw, 0.0f);
+
+	FVector ForwardDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	FVector RightDir = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	UCharacterMovementComponent* MovementComp = Character->GetCharacterMovement();
+
+	if (!MovementComp)
+	{
+		return Character->GetActorForwardVector();
+	}
+
+	FVector LastInput = MovementComp->GetLastInputVector();
+
+	if (LastInput.IsNearlyZero())
+	{
+		return Character->GetActorForwardVector();
+	}
+
+	return LastInput.GetSafeNormal();
+}
---
+++ Source/SimpleRPG/AbilitySystem/Abilities/GA_Dash.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SimpleRPGGameplayAbility.h"
+#include "GA_Dash.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLERPG_API UGA_Dash : public USimpleRPGGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+
+	UGA_Dash();
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* Actorinfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* Actorinfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+
+private:
+
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	UFUNCTION()
+	void OnMontageCancelled();
+
+	FVector GetDashDirection() const;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "SimpleRPG|Dash")
+	TObjectPtr<UAnimMontage> DashMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SimpleRPG|Dash")
+	float DashDistance = 600.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SimpleRPG|Dash")
+	float DashDuration = 0.3f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SimpleRPG|Dash")
+	TSubclassOf<UGameplayEffect> CoolDownEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SimpleRPG|Dash")
+	TSubclassOf<UGameplayEffect> BlockMovementEffect;
+
+	FActiveGameplayEffectHandle BlockMovementEffectHandle;
+	
+};
