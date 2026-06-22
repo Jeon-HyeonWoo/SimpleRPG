b15de69ed082e1fc2d4d4c03231542737d813cde
현우 전 <jhw096@gmail.com>
2026-06-22 15:17:01 +0900

    feat : Damage, SetByCaller 태그 추가
    - 데이미 ExecCalc용 Native Tag 추가 및 불필요 API 매크로 삭제

--- Source/SimpleRPG/SimpleRPGGameplayTag.cpp
+++ Source/SimpleRPG/SimpleRPGGameplayTag.cpp
@@ -5,5 +5,23 @@
 
 namespace SimpleRPGGameplayTags
 {
-	SIMPLERPG_API UE_DEFINE_GAMEPLAY_TAG(State_Movement_Blocked, "State.Movement.Blocked");
+#pragma region State Tag
+
+	UE_DEFINE_GAMEPLAY_TAG(State_Movement_Blocked, "State.Movement.Blocked");
+
+#pragma endregion
+	
+#pragma region SetByCaller Tag
+
+	UE_DEFINE_GAMEPLAY_TAG(SetByCaller_Skill_Ratio, "SetByCaller.Skill.Ratio");
+
+#pragma endregion
+
+#pragma region Damage Tag
+
+	UE_DEFINE_GAMEPLAY_TAG(Damage_Type_AD, "Damage.Type.AD");
+	UE_DEFINE_GAMEPLAY_TAG(Damage_Type_AP, "Damage.Type.AP");
+
+#pragma endregion
+
 }
