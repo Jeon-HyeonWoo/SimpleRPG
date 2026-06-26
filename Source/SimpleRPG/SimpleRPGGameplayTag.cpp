290c0a29a5ecd072050ac1a395515b3afa5cd45b
현우 전 <jhw096@gmail.com>
2026-06-27 02:24:22 +0900

    Refactor : Event 도메인 재편 Native 태그 추가

--- Source/SimpleRPG/SimpleRPGGameplayTag.cpp
+++ Source/SimpleRPG/SimpleRPGGameplayTag.cpp
@@ -5,30 +5,37 @@
 
 namespace SimpleRPGGameplayTags
 {
-#pragma region State Tag
-
-	UE_DEFINE_GAMEPLAY_TAG(State_Movement_Blocked, "State.Movement.Blocked");
-
+	//State
 	UE_DEFINE_GAMEPLAY_TAG(State_Dashing, "State.Dasing");
 
+	//State_Movement
+	UE_DEFINE_GAMEPLAY_TAG(State_Movement_Blocked, "State.Movement.Blocked");
+
+	//State_Acting
 	UE_DEFINE_GAMEPLAY_TAG(State_Acting, "State.Acting");
 	UE_DEFINE_GAMEPLAY_TAG(State_Acting_Attacking, "State.Acting.Attacking");
 	UE_DEFINE_GAMEPLAY_TAG(State_Acting_Swapping, "State.Acting.Swapping");
 	
 
-#pragma endregion
-	
-#pragma region SetByCaller Tag
-
+	//SetByCaller
 	UE_DEFINE_GAMEPLAY_TAG(SetByCaller_Skill_Ratio, "SetByCaller.Skill.Ratio");
 
-#pragma endregion
-
-#pragma region Damage Tag
-
+	//Damage
 	UE_DEFINE_GAMEPLAY_TAG(Damage_Type_AD, "Damage.Type.AD");
 	UE_DEFINE_GAMEPLAY_TAG(Damage_Type_AP, "Damage.Type.AP");
 
-#pragma endregion
+
+	//Event_Combat Shared
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_HitDetect, "Event.Combat.HitDetect");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_Damaged, "Event.Combat.Damaged");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_Death, "Event.Combat.Death");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_ComboWindow_Open, "Event.Combat.ComboWindow.Open");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_ComboWindow_Close, "Event.Combat.ComboWindow.Close");
+	UE_DEFINE_GAMEPLAY_TAG(Event_Combat_AttackTrigger, "Event.Combat.AttackTrigger");
+
+
+	//Evnet_Skill_Sword
+	UE_DEFINE_GAMEPLAY_TAG(Event_Skill_Sword_PowerUp, "Event.Skill.Sword.PowerUp");
+
 
 }
