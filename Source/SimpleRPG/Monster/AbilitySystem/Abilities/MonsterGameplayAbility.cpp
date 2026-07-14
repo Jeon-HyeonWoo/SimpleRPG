ef9731446dfed5f6e713ffe00220bfd78117d170
현우 전 <jhw096@gmail.com>
2026-07-14 22:12:14 +0900

    feat : Monster GA공통 부모 UMonsterGameplayAbility 추가
    - Montage 재생 절차를 부모로 통합
    - MonsterData의 ActionDataMap에서 행동 태그로 Montage Damage 조회
    - MontageTask를 멤버로 보관하여 재발동시 이전 Task정리
    (Stagger 재 피격 대응)

---
+++ Source/SimpleRPG/Monster/AbilitySystem/Abilities/MonsterGameplayAbility.cpp
@@ -0,0 +1,104 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonsterGameplayAbility.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "SimpleRPG/Monster/SimpleRPGMonsterBase.h"
+#include "SimpleRPG/Monster/Data/MonsterActionData.h"
+#include "SimpleRPG/Monster/Data/MonsterData.h"
+
+ASimpleRPGMonsterBase* UMonsterGameplayAbility::GetOwningMonster() const
+{
+	
+	ASimpleRPGMonsterBase* MonsterCharacter = Cast<ASimpleRPGMonsterBase>(GetAvatarActorFromActorInfo());
+	if (!MonsterCharacter)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("MonsterCharacter invalid : %d, %hs"), __LINE__, __FUNCTION__);
+		return nullptr;
+	}
+	
+	return MonsterCharacter;
+}
+
+const FMonsterMontageEntry* UMonsterGameplayAbility::GetRandomMontageEntry(const FGameplayTag& ActionTag) const
+{
+	ASimpleRPGMonsterBase* Monster = GetOwningMonster();
+	if (!Monster)
+	{
+		return nullptr;
+	}
+
+	UMonsterData* MonsterData = GetOwningMonster()->GetMonsterData();
+	if (!MonsterData)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("MonsterData invalid : %d, %hs"), __LINE__, __FUNCTION__);
+		return nullptr;
+	}
+	
+	const FMonsterActionData* ActionData = MonsterData->ActionDataMap.Find(ActionTag);
+	if (!ActionData)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("MonsterActionData invalid : %d, %hs"), __LINE__, __FUNCTION__);
+		return nullptr;
+	}
+
+	return ActionData->GetRandomEntry();
+}
+
+bool UMonsterGameplayAbility::PlayActionMontage(const FGameplayTag& ActionTag)
+{
+	const FMonsterMontageEntry* Entry = GetRandomMontageEntry(ActionTag);
+	if (!Entry)
+	{
+		return false;
+	}
+
+	CurrentEntry = Entry;
+
+	UAnimMontage* Montage = Entry->Montage;
+	if (!Montage)
+	{
+		return false;
+	}
+
+	if (MontageTask)
+	{
+		MontageTask->EndTask();
+	}
+
+	UAbilityTask_PlayMontageAndWait* Task = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+		this,
+		NAME_None,
+		Montage,
+		1.0f
+	);
+
+	Task->OnCompleted.AddDynamic(this, &UMonsterGameplayAbility::HandleMontageCompleted);
+	Task->OnCancelled.AddDynamic(this, &UMonsterGameplayAbility::HandleMontageCancelled);
+
+	MontageTask = Task;
+
+	Task->ReadyForActivation();
+
+	return true;
+}
+
+void UMonsterGameplayAbility::HandleMontageCompleted()
+{
+	OnActionMontageCompleted();
+}
+
+void UMonsterGameplayAbility::HandleMontageCancelled()
+{
+	OnActionMontageCancelled();
+}
+
+void UMonsterGameplayAbility::OnActionMontageCompleted()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UMonsterGameplayAbility::OnActionMontageCancelled()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
---
+++ Source/SimpleRPG/Monster/AbilitySystem/Abilities/MonsterGameplayAbility.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SimpleRPG/AbilitySystem/Abilities/SimpleRPGGameplayAbility.h"
+#include "SimpleRPG/Monster/Data/MonsterActionData.h"
+#include "MonsterGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+class ASimpleRPGMonsterBase;
+class UAbilityTask_PlayMontageAndWait;
+
+UCLASS()
+class SIMPLERPG_API UMonsterGameplayAbility : public USimpleRPGGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+
+	ASimpleRPGMonsterBase* GetOwningMonster() const;
+	const FMonsterMontageEntry* GetRandomMontageEntry(const FGameplayTag& ActionTag) const;
+
+	bool PlayActionMontage(const FGameplayTag& ActionTag);
+
+	UFUNCTION()
+	void HandleMontageCompleted();
+	UFUNCTION()
+	void HandleMontageCancelled();
+
+	virtual void OnActionMontageCompleted();
+	virtual void OnActionMontageCancelled();
+
+protected:
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_PlayMontageAndWait> MontageTask;
+
+	const FMonsterMontageEntry* CurrentEntry = nullptr;
+};
